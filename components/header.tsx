1ad5ed372720f5038a5e1c743e82982e576e2ebc
Akhil <akhil1659@gmail.com>
2025-04-11 16:04:51 +0530

    Header section completed

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,31 @@
 const Header = () => {
     return ( 
         <div className=" w-full h-full mt-20">
-          <div className="flex justify-center relative">
-            <img
-              src="./vector.svg"
-              alt="Vector background"
-              className="absolute left-1/2 -top-12 z-10 opacity-80"
-            />
-            <div  className="flex flex-col gap-y-2 z-20">
-              <div className="text-3xl font-semibold text-[#FEF7EE] text-center">
-                Our Collection
-              </div>
-              <div className="text-[#6F757C] text-center max-w-xl">
-                Introducing our Coffee Collection, a selection of unique coffees <br />
-                from different roast types and origins, expertly roasted in small <br />
-                batches and shipped fresh weekly.
-              </div>
+            <div className="flex justify-center relative">
+                <img
+                src="./vector.svg"
+                alt="Vector background"
+                className="absolute left-1/2 -top-12 z-10 opacity-80 pointer-events-none"
+                />
+                <div  className="flex flex-col gap-y-2 z-20">
+                    <div className="text-3xl font-semibold text-[#FEF7EE] text-center">
+                        Our Collection
+                    </div>
+                    <div className="text-[#6F757C] text-center max-w-xl">
+                        Introducing our Coffee Collection, a selection of unique coffees <br />
+                        from different roast types and origins, expertly roasted in small <br />
+                        batches and shipped fresh weekly.
+                    </div>
+                    <div className="flex justify-center gap-x-4 w-full mt-2">
+                        <div className="bg-[#4D5562] px-4 py-1.5 cursor-pointer rounded-md">
+                            All Products
+                        </div>
+                        <div className="px-4 py-1.5 cursor-pointer rounded-md">
+                            Available Now
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
      );
 }
